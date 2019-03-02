e48c43b8e85e2cb67e3da2f86f7c006b7f049bad
VaryaKasha <varyaxx@gmail.com>
2019-03-02 23:20:51 +0200

    fixed and updated support vectors code

--- support_vector_machines.py
+++ support_vector_machines.py
@@ -1,11 +1,12 @@
 import sys
 from memory_profiler import profile
 import pandas
-import time
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_predict, cross_validate
 from sklearn.svm import SVR
 from sklearn.preprocessing import RobustScaler
+from statistics import mean
+import pickle
 
 
 @profile
@@ -19,25 +20,23 @@ def svm():
     X = rbX.fit_transform(X)
     rbY = RobustScaler()
     y = rbY.fit_transform(y.values.reshape(-1, 1))
-    # y = column_or_1d(y, warn=True)
 
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33)
 
-    start = time.time()
     # kernel: rbf, sigmoid, linear, poly   gamma=0.1, C=100, epsilon=0.1
-    model = SVR(kernel='rbf')
-    model.fit(X_train, y_train.ravel())
-    end = time.time()
+    model = SVR(kernel='rbf', gamma='auto')
 
-    predictions = model.predict(rbX.transform(X_test))
-    # predictions = rbY.inverse_transform(predictions.reshape(-1, 1))
+    scores = cross_validate(model, X_train, y_train.ravel(), cv=10, return_train_score=True)
+    predictions = cross_val_predict(model, rbX.transform(X_test), y_test.ravel(), cv=10)
 
-    print('Час побудови моделі (мс): ', end - start)
-    print('Об’єм пам’яті, займаємий моделю (MiB): ', sys.getsizeof(model))
-    print('Помилка моделі для навчальної вибірки (%): ', 100 - model.score(X_train, y_train) * 100)
-    print('Помилка моделі для тестової вибірки (%): ', 100 - model.score(X_test, y_test) * 100)
+    print(scores)
 
-    # y_test = rbY.inverse_transform(y_test)
+    p = pickle.dumps(model)
+    model_size = sys.getsizeof(p)
+    print('Час побудови моделі (мс): ', mean(scores['fit_time']))
+    print('Об’єм пам’яті, займаємий моделю (MiB): ', model_size)
+    print('Помилка моделі для навчальної вибірки (%): ', 100 - mean(scores['train_score']) * 100)
+    print('Помилка моделі для тестової вибірки (%): ', 100 - mean(scores['test_score']) * 100)
 
     plt.scatter(y_test, predictions)
     plt.xlabel("Реальні значення")
